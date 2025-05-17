841ede2b968679346246992e9dedc20686fe7f33
MD SHAHFAHAD KHAN <168389196+shahfahad09@users.noreply.github.com>
2025-05-17 18:16:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,59 @@
+# # app.py
+# from flask import Flask, request, jsonify, render_template
+# from keras.models import load_model
+# import numpy as np
+# import joblib
+
+# app = Flask(__name__)
+
+# # Load model and scalers once at startup
+# model = load_model("house_rent_lstm_model.h5")
+# scaler_x = joblib.load("scaler_x.save")
+# scaler_y = joblib.load("scaler_y.save")
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     data = request.form
+#     try:
+#         features = np.array([
+#             float(data['bhk']),
+#             float(data['size']),
+#             float(data['area_type']),
+#             float(data['city']),
+#             float(data['furnishing_status']),
+#             float(data['tenant_preferred']),
+#             float(data['bathroom'])
+#         ]).reshape(1, -1)
+
+#         features_scaled = scaler_x.transform(features)
+#         features_scaled = features_scaled.reshape((features_scaled.shape[0], features_scaled.shape[1], 1))
+
+#         prediction_scaled = model.predict(features_scaled)
+#         prediction = scaler_y.inverse_transform(prediction_scaled)
+
+#         rent = float(round(prediction[0][0], 2))  # <-- yahan float conversion
+
+#         return jsonify({'predicted_rent': rent})
+
+#     except Exception as e:
+#         return jsonify({'error': str(e)})
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+
 # app.py
 from flask import Flask, request, jsonify, render_template
 from keras.models import load_model
 import numpy as np
 import joblib
+import os
 
 app = Flask(__name__)
 
@@ -14,6 +65,7 @@ scaler_y = joblib.load("scaler_y.save")
 @app.route('/')
 def home():
     return render_template('index.html')
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.form
@@ -34,13 +86,15 @@ def predict():
         prediction_scaled = model.predict(features_scaled)
         prediction = scaler_y.inverse_transform(prediction_scaled)
 
-        rent = float(round(prediction[0][0], 2))  # <-- yahan float conversion
+        rent = float(round(prediction[0][0], 2))
 
         return jsonify({'predicted_rent': rent})
 
     except Exception as e:
         return jsonify({'error': str(e)})
 
-
+#  Updated for render deployment
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
